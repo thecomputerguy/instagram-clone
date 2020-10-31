f1896b78f413e03bb8060d5eb34e1044b7348287
Varun Sharma <varunsharma12@outlook.com>
2020-10-31 23:45:51 +0530

    Modified base files.

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,8 +4,7 @@ import 'firebase/auth'
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    //apiKey: process.env.REACT_APP_APP_ID,
-    apiKey: 'AIzaSyAwXbTlunL-BLZuLFGLfwl1gXAIkIF57sY',
+    apiKey: process.env.REACT_APP_APP_ID,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
