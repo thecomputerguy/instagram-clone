c560175561a1633d4cb625cd54e72172baffac4e
Varun Sharma <varunsharma12@outlook.com>
2020-11-01 00:14:23 +0530

    😀 Changed API Key.

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,7 +4,7 @@ import 'firebase/auth'
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: process.env.REACT_APP_APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
